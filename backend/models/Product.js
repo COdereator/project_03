772d61b54c5c6bcf72082e187b4b7c0e578201cd
rohittt37 <rohitttgupta808@gmail.com>
2025-03-26 11:50:17 +0530

    done

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,98 +1,47 @@
 import mongoose from 'mongoose';
 
-const reviewSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5
-  },
-  comment: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-});
-
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    trim: true
-  },
-  description: {
-    type: String,
-    required: true
   },
   price: {
     type: Number,
     required: true,
-    min: 0
+  },
+  description: {
+    type: String,
+    required: true,
   },
   category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  images: [{
     type: String,
     required: true,
-    trim: true
-  },
-  image: {
+  }],
+  sizes: [{
     type: String,
-    required: true
-  },
+    required: true,
+  }],
+  colors: [{
+    type: String,
+    required: true,
+  }],
+  features: [{
+    type: String,
+  }],
   stock: {
     type: Number,
-    required: true,
-    min: 0,
-    default: 0
-  },
-  rating: {
-    type: Number,
-    required: true,
     default: 0,
-    min: 0,
-    max: 5
   },
-  reviews: [reviewSchema]
-}, {
-  timestamps: true
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
-
-// Virtual for formatted price
-productSchema.virtual('formattedPrice').get(function() {
-  return `$${this.price.toFixed(2)}`;
-});
-
-// Method to check if product is in stock
-productSchema.methods.isInStock = function() {
-  return this.stock > 0;
-};
-
-// Method to update stock
-productSchema.methods.updateStock = async function(quantity) {
-  if (this.stock >= quantity) {
-    this.stock -= quantity;
-    await this.save();
-    return true;
-  }
-  return false;
-};
-
-// Method to add a review
-productSchema.methods.addReview = async function(userId, rating, comment) {
-  this.reviews.push({ user: userId, rating, comment });
-  
-  // Update average rating
-  const totalRating = this.reviews.reduce((sum, review) => sum + review.rating, 0);
-  this.rating = totalRating / this.reviews.length;
-  
-  await this.save();
-  return this;
-};
 
 const Product = mongoose.model('Product', productSchema);
 export default Product;
